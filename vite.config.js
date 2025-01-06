48c44dee127e8242048e0a22b96f70e96b0aa3de
PranavGediya <pranavgomail8743@gmail.com>
2025-01-06 19:52:33 +0530

    commit
    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // Set alias to point to the src directory
+    },
+  },
 server: {
   host: true,
   port: 80,
